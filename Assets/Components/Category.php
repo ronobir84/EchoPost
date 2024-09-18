e2971b27c9275e98689fc355de84ca1f78c81786
ronodir84 <priashdas@84gmail.com>
2024-09-18 21:52:08 +0600

    add this last category section

--- Assets/Components/Category.php
+++ Assets/Components/Category.php
@@ -1,22 +1,100 @@
 <section>
-    <div>
+    <div class="px-16 mt-20">
         <!-- flex div -->
         <div>
             <!-- category section -->
             <div>
+
                 <!-- Topics Index -->
-                 <div>
-                    <img src="" alt="">
-                 </div>
-
-                <div>
-
+                <div class="flex items-center gap-2 pl-5">
+                    <img class="w-5 h-5" src="http://localhost/EchoPost/Assets/Images/footer_image/pink-star (1).png" alt="">
+                    <h2 class="text-2xl font-bold text-black ">Topics Index </h2>
                 </div>
+
+                <div class="px-16">
+                    <!-- sidebar start -->
+                    <div class="  max-w-2xl   py-2 flex flex-col md:flex-row ">
+
+                        <ul class="basis-1/2">
+                            <!-- first category section -->
+                            <li>
+                                <div class="w-8 h-8 bg-blue-700 rounded relative right-11 top-[32px]">
+                                    <h3 class="text-lg font-bold text-white pl-3 pt-0.5 shadow-sm ">1</h3>
+                                </div>
+                                <button class="relative  flex gap-2 items-center w-full  font-semibold text-left  md:text-lg  " aria-expanded="false" onclick="toggleFAQ(this)">
+                                    <span class="flex-1 text-2xl text-black font-bold">Fundamentals</span>
+                                    <i class="fa-solid fa-turn-down relative right-36 text-xl font-bold text-black"></i>
+                                </button>
+                                <div class="transition-all duration-500 ease-in-out border-l-[3px] mt-4 border-gray-300 max-h-0 overflow-hidden" style="transition: max-height 0.3s ease-in-out 0s;">
+                                    <ol class="list-decimal relative left-8 ">
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                    </ol>
+                                </div>
+                            </li>
+
+
+                            <!-- Second category section -->
+                            <li>
+                                <div class="w-8 h-8 bg-orange-400 rounded relative right-11 top-[32px]">
+                                    <h3 class="text-lg font-bold text-white pl-3 pt-0.5 shadow-sm ">2</h3>
+                                </div>
+                                <button class="relative  flex gap-2 items-center w-full  font-semibold text-left  md:text-lg  " aria-expanded="false" onclick="toggleFAQ(this)">
+                                    <span class="flex-1 text-2xl text-black font-bold">HTML</span>
+                                    <i class="fa-solid fa-turn-down relative right-36 text-xl font-bold text-black"></i>
+                                </button>
+                                <div class="transition-all duration-500 ease-in-out border-l-[3px] mt-4 border-gray-300 max-h-0 overflow-hidden" style="transition: max-height 0.3s ease-in-out 0s;">
+                                    <ol class="list-decimal relative left-8 ">
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                    </ol>
+                                </div>
+                            </li>
+
+
+                            <!-- third category section -->
+                            <li>
+                                <div class="w-8 h-8 bg-purple-700 rounded relative right-11 top-[32px]">
+                                    <h3 class="text-lg font-bold text-white pl-3 pt-0.5 shadow-sm ">3</h3>
+                                </div>
+                                <button class="relative  flex gap-2 items-center w-full  font-semibold text-left  md:text-lg  " aria-expanded="false" onclick="toggleFAQ(this)">
+                                    <span class="flex-1 text-2xl text-black font-bold">Javascript</span>
+                                    <i class="fa-solid fa-turn-down relative right-36 text-xl font-bold text-black"></i>
+                                </button>
+                                <div class="transition-all duration-500 ease-in-out border-l-[3px] mt-4 border-gray-300 max-h-0 overflow-hidden" style="transition: max-height 0.3s ease-in-out 0s;">
+                                    <ol class="list-decimal relative left-8 ">
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                        <li>Hello</li>
+                                    </ol>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <script>
+                        function toggleFAQ(button) {
+                            const content = button.nextElementSibling;
+                            button.setAttribute("aria-expanded", button.getAttribute("aria-expanded") === "false" ? "true" : "false");
+                            content.style.maxHeight = button.getAttribute("aria-expanded") === "true" ? content.scrollHeight + "px" : "0";
+                        }
+                    </script>
+                </div>
+                <!-- sidebar end -->
 
             </div>
             <!-- post section -->
 
             <div>
+
+            
+
+
 
             </div>
         </div>
