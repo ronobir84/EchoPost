310af3afa36c0043d93d20c8d682b28ef7c187d2
ronodir84 <priashdas@84gmail.com>
2024-09-27 19:46:34 +0600

    add this last all item

--- Assets/Components/trending_topics.php
+++ Assets/Components/trending_topics.php
@@ -1,9 +1,11 @@
 <div class="mt-20">
     <?php
+    include_once('./database.php');
     $sql = "SELECT * FROM categories";
     $query = mysqli_query($database, $sql);
-    
-    
+    $rows = mysqli_num_rows($query);
+
+
     ?>
     <div class="flex justify-center  items-center">
         <img class="w-5 h-5" src="http://localhost/EchoPost/Assets/Images/footer_image/pink-star (1).png" alt="">
@@ -11,48 +13,33 @@
         <h2 class="text-xl text-black font-bold text-center">Trending Topics</h2>
     </div>
     <div class="w-10/12 bg-white shadow-md mt-10  rounded-full py-3 mx-auto ">
+
         <div class="flex justify-between px-16">
+
             <!-- first category -->
-            <div>
-                <div class="w-24 h-24  shadow-xl hover:shadow-orange-500/100 duration-500  rounded-full bg-orange-400">
+            <?php
+            if ($rows) {
+                while ($row = mysqli_fetch_assoc($query)) {
 
 
-                </div>
-                <h2 class="text-xl font-bold text-black pl-3 pt-1">HTML</h2>
-            </div>
-
-            <!-- second category -->
-            <div>
-                <div class="w-24 h-24  shadow-xl hover:shadow-purple-700/100 duration-500 bg-purple-700 rounded-full ">
 
 
-                </div>
-                <h3 class="text-xl font-bold text-black relative right-6 pt-1">Fundamentals</h3>
-            </div>
-            <!-- third category -->
-            <div>
-                <div class="w-24 h-24  rounded-full shadow-xl hover:shadow-blue-700/100 duration-500 bg-blue-700">
+            ?>
+                    <div class="w-32 border ">
+
+                        <div class="w-24 h-24 mx-auto shadow-xl hover:shadow-[<?php echo $row['category_color'] ?>] duration-500  rounded-full bg-[<?php echo $row['category_color'] ?>]">
 
 
-                </div>
-                <h1 class="text-xl font-bold text-black relative left-6 pt-1">CSS</h1>
-            </div>
-            <!-- Four category -->
-            <div>
-                <div class="w-24 h-24  shadow-xl hover:shadow-blue-500/100 bg-blue-500 rounded-full ">
+                        </div>
+                        <h2 class="text-xl font-bold text-black text-center  pt-1"><?php echo $row['category_name']?></h2>
+
+                    </div>
 
 
-                </div>
-                <h2 class="text-xl font-bold text-black relative right-2 pt-1">Databases</h2>
-            </div>
-            <!-- five category -->
-            <div>
-                <div class="w-24 h-24  shadow-xl hover:shadow-black/100 duration-500 bg-black rounded-full ">
+            <?php  }
+            } ?>
 
 
-                </div>
-                <h3 class="text-xl font-bold text-black relative right-3 pt-1">Deployment</h3>
-            </div>
             <div class="flex gap-9 items-center">
                 <h3 class="text-xl font-semibold text-black">or....</h3>
                 <button class="  w-36 h-12  md:block  rounded hover:bg-[#282424] duration-500 text-lg font-semibold text-white bg-[#6A4EE9]">
@@ -60,6 +47,7 @@
                 </button>
 
             </div>
+
 
         </div>
 
