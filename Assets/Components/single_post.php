275a78293b95ecb0504065fe95f77b2f833a1fda
ronodir84 <priashdas@84gmail.com>
2024-09-28 14:57:07 +0600

    css file add this

--- Assets/Components/single_post.php
+++ Assets/Components/single_post.php
@@ -5,29 +5,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EchoPost</title>
-    <link rel="stylesheet" href="http://localhost/EchoPost/Assets/Css/single_post.css">
+    <link rel="stylesheet" href="../../Assets/Css/single_post.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Playwrite+AU+VIC:wght@100..400&family=Playwrite+NZ:wght@100..400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+
+
+
+
+
+
+
+    <style>
+        .banner_1 {
+            width: 100%;
+            height: 350px;
+            background-color: black;
+
+        }
+
+        .banner_text {
+            color: #FFFFFF;
+
+            font-size: 35px;
+
+            font-family: "Noto Serif", serif;
+
+            font-weight: 700;
+            text-align: center;
+
+
+        }
+
+         .dev {
+            display: inline-block;
+            font-size: 35px;
+            line-height: 1;
+            font-weight: black;
+            background: linear-gradient(to top left, #ff75c3, #ffa647, #ffe83f, #9fff5b, #70e2ff, #cd93ff);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            font-family: "Noto Serif", serif;
+
+        }
+    </style>
+
 
 </head>
 
 <body>
     <!-- banner section first -->
-     <section>
-    
-            <div>
-                <div class="banner_1">
-                    <div>
-                         
+    <section>
 
-                    </div>
-                    <div></div>
+        <div>
+            <div class="banner_1">
+                <h1 class="banner_text">Every <span class="dev">developer</span> has a tab open to EchoPost</h1>
 
-
-                </div>
 
 
             </div>
-    
-     </section>
+
+
+        </div>
+
+    </section>
 
 
 
