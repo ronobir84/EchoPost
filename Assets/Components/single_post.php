a151db9053333fd4a3057b4ab86c7caf943f0d58
ronodir84 <priashdas@84gmail.com>
2024-09-28 16:05:24 +0600

    banner section

--- Assets/Components/single_post.php
+++ Assets/Components/single_post.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EchoPost</title>
-    <link rel="stylesheet" href="../../Assets/Css/single_post.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
 
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Playwrite+AU+VIC:wght@100..400&family=Playwrite+NZ:wght@100..400&display=swap" rel="stylesheet">
@@ -19,6 +19,24 @@
 
 
     <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+             
+            scroll-behavior: smooth;
+
+        }
+
+        body {
+            background-color: #F5F5F5;
+            width: 100%;
+            min-height: 100vh;
+            max-height: auto;
+            font-family: "Nunito Sans", sans-serif;
+
+        }
+
         .banner_1 {
             width: 100%;
             height: 350px;
@@ -29,19 +47,20 @@
         .banner_text {
             color: #FFFFFF;
 
-            font-size: 35px;
+            font-size: 40px;
 
             font-family: "Noto Serif", serif;
 
             font-weight: 700;
             text-align: center;
+            padding-top: 40px;
 
 
         }
 
-         .dev {
+        .dev {
             display: inline-block;
-            font-size: 35px;
+            font-size: 40px;
             line-height: 1;
             font-weight: black;
             background: linear-gradient(to top left, #ff75c3, #ffa647, #ffe83f, #9fff5b, #70e2ff, #cd93ff);
@@ -49,6 +68,11 @@
             -webkit-text-fill-color: transparent;
             font-family: "Noto Serif", serif;
 
+        }
+
+        .text-pr {
+            text-align: center;
+            color: white;
         }
     </style>
 
@@ -62,6 +86,7 @@
         <div>
             <div class="banner_1">
                 <h1 class="banner_text">Every <span class="dev">developer</span> has a tab open to EchoPost</h1>
+                <h4 class="text-pr">For over New we’ve been the Q&A platform of choice that millions of people visit every month to ask questions, learn, and share technical knowledge</h4>
 
 
 
