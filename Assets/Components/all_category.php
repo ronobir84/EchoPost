e2783d096601136b2458a9aec8bffdc8a2b88558
ronodir84 <priashdas@84gmail.com>
2024-10-14 13:41:57 +0600

    add this add the min last section

--- Assets/Components/all_category.php
+++ Assets/Components/all_category.php
@@ -11,6 +11,21 @@
 
 
 <div class="bg-[#F3F4F6] min-h-screen">
+    <div class="text-center relative top-12">
+        <h2 class="text-5xl font-black text-black">Explore our Categories ✨</h2>
+        <p class="text-lg font-medium text-gray-800 w-[45%] mx-auto mt-6">Whether you’re a photography aficionado or simply intrigued by the art of visual storytelling, our blog is your gateway to exploring the mesmerizing world of renowned photographers and the captivating narratives.</p>
+    </div>
+
+
+    <div class="mx-36 mt-24 h-96 bg-white">
+        <div>
+            <!--  -->
+            <div>
+
+            </div>
+        </div>
+
+    </div>
 
 </div>
 
