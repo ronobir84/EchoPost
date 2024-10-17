764e90ba487c8f023c053c68646cd45c236febd8
ronodir84 <priashdas@84gmail.com>
2024-10-17 20:06:00 +0600

    overview section and last min section

--- Admin/index.php
+++ Admin/index.php
@@ -7,6 +7,7 @@
 <div>
     <div class="w-full min-h-screen bg-[#E5ECFF]">
         <div class="pt-10 px-8">
+            <!-- overview -->
             <div class="flex justify-between items-center">
                 <div>
                     <h3 class="text-2xl font-bold text-[#6A4EE9]">Overview</h3>
@@ -15,6 +16,27 @@
                     <i class="fa-solid fa-comment fa-2xl text-[#6A4EE9]"></i>
                     <i class="fa-solid fa-bell fa-2xl text-[#6A4EE9]"></i>
                 </div>
+
+
+            </div>
+            <!-- div card section all details -->
+            <div>
+
+                <div class="w-80 h-32 bg-white shadow-md rounded-sm">
+                    <div class="flex justify-between px-5 pt-6 items-center">
+
+                        <div class="w-20 h-20 rounded-full bg-pink-600 p-[21px]">
+                            <i class="fa-solid fa-rss text-4xl text-white"></i>
+                        </div>
+                        <div class="border-l-4 border-[#6A4EE9] pl-11">
+                            <h2 class="text-xl font-bold text-black">Blogs</h2>
+                            <h1 class="text-3xl font-black text-[#6A4EE9]">10</h1>
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
         </div>
 
