afd8e0707d2a6e18f54827e823cd8e9f56d029bc
ronodir84 <priashdas@84gmail.com>
2024-10-24 11:46:51 +0600

    add this single category section

--- Assets/Components/category_single.php
+++ Assets/Components/category_single.php
@@ -28,8 +28,9 @@
 
 
     <div>
-        <div>
-            <div class="w-[450px] h-96 border-2 border-black">
+        <div class="grid grid-cols-3 px-40 mt-14 gap-5">
+            <!-- first card -->
+            <div class="w-[420px] pb-6 border-2 border-gray-300 bg-[#FFFFFF]">
                 <div class="mt-5 flex justify-between">
                     <div>
                         <button class="px-5 py-2.5 bg-orange-400 text-sm font-bold text-white hover:px-7 duration-500 rounded-r-full">HTML</button>
@@ -51,7 +52,132 @@
                 <div class="w-96 text-center mx-auto pt-7">
                     <p class="text-lg font-medium text-gray-500">We’ve seen the basics of selectors.CSS selectors offer versatile ways to target specific HTML elements for styling….</p>
                 </div>
-                
+                <div class="flex w-72 mx-auto gap-6 items-center   mt-5">
+                    <div class="flex gap-3 items-center">
+                        <img class="w-8 h-8 rounded-full" src="https://ronobirdev.surge.sh/assets/ronobir-cbde17b2.png" alt="">
+                        <h2 class="text-lg font-semibold text-black">Ronobir</h2>
+                    </div>
+                    <div class="flex gap-2 ">
+                        <i class="fa-solid fa-stopwatch text-lg text-[#6A4EE9]"></i>
+
+                        <h2 class="text-lg text-[#6A4EE9] font-medium"> 2 Min Read</h2>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <!-- first card -->
+            <div class="w-[420px] pb-6 border-2 border-gray-300 bg-[#FFFFFF]">
+                <div class="mt-5 flex justify-between">
+                    <div>
+                        <button class="px-5 py-2.5 bg-orange-400 text-sm font-bold text-white hover:px-7 duration-500 rounded-r-full">HTML</button>
+                    </div>
+                    <div class="flex items-center  gap-2 pr-5">
+                        <i class="fa-solid fa-award  text-blue-700 fa-lg"></i>
+                        <h4 class=" text-blue-700">Difficulty : <span class="fa fa-star checked text-orange-400"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star text-orange-400"></span>
+                        </h4>
+                    </div>
+                </div>
+                <div class="w-72 text-center mx-auto pt-6">
+                    <a href="">
+                        <h1 class="text-2xl font-bold text-[#282424] hover:underline duration-700">Crafting Engaging CSS Animations step by step guide</h1>
+                    </a>
+                </div>
+                <div class="w-96 text-center mx-auto pt-7">
+                    <p class="text-lg font-medium text-gray-500">We’ve seen the basics of selectors.CSS selectors offer versatile ways to target specific HTML elements for styling….</p>
+                </div>
+                <div class="flex w-72 mx-auto gap-6 items-center   mt-5">
+                    <div class="flex gap-3 items-center">
+                        <img class="w-8 h-8 rounded-full" src="https://ronobirdev.surge.sh/assets/ronobir-cbde17b2.png" alt="">
+                        <h2 class="text-lg font-semibold text-black">Ronobir</h2>
+                    </div>
+                    <div class="flex gap-2 ">
+                        <i class="fa-solid fa-stopwatch text-lg text-[#6A4EE9]"></i>
+
+                        <h2 class="text-lg text-[#6A4EE9] font-medium"> 2 Min Read</h2>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <!--  second card -->
+            <div class="w-[420px] pb-6 border-2 border-gray-300 bg-[#FFFFFF]">
+                <div class="mt-5 flex justify-between">
+                    <div>
+                        <button class="px-5 py-2.5 bg-orange-400 text-sm font-bold text-white hover:px-7 duration-500 rounded-r-full">HTML</button>
+                    </div>
+                    <div class="flex items-center  gap-2 pr-5">
+                        <i class="fa-solid fa-award  text-blue-700 fa-lg"></i>
+                        <h4 class=" text-blue-700">Difficulty : <span class="fa fa-star checked text-orange-400"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star text-orange-400"></span>
+                        </h4>
+                    </div>
+                </div>
+                <div class="w-72 text-center mx-auto pt-6">
+                    <a href="">
+                        <h1 class="text-2xl font-bold text-[#282424] hover:underline duration-700">Crafting Engaging CSS Animations step by step guide</h1>
+                    </a>
+                </div>
+                <div class="w-96 text-center mx-auto pt-7">
+                    <p class="text-lg font-medium text-gray-500">We’ve seen the basics of selectors.CSS selectors offer versatile ways to target specific HTML elements for styling….</p>
+                </div>
+                <div class="flex w-72 mx-auto gap-6 items-center   mt-5">
+                    <div class="flex gap-3 items-center">
+                        <img class="w-8 h-8 rounded-full" src="https://ronobirdev.surge.sh/assets/ronobir-cbde17b2.png" alt="">
+                        <h2 class="text-lg font-semibold text-black">Ronobir</h2>
+                    </div>
+                    <div class="flex gap-2 ">
+                        <i class="fa-solid fa-stopwatch text-lg text-[#6A4EE9]"></i>
+
+                        <h2 class="text-lg text-[#6A4EE9] font-medium"> 2 Min Read</h2>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- third card -->
+            <div class="w-[420px] pb-6 border-2 border-gray-300 bg-[#FFFFFF]">
+                <div class="mt-5 flex justify-between">
+                    <div>
+                        <button class="px-5 py-2.5 bg-orange-400 text-sm font-bold text-white hover:px-7 duration-500 rounded-r-full">HTML</button>
+                    </div>
+                    <div class="flex items-center  gap-2 pr-5">
+                        <i class="fa-solid fa-award  text-blue-700 fa-lg"></i>
+                        <h4 class=" text-blue-700">Difficulty : <span class="fa fa-star checked text-orange-400"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star checked text-orange-400 checked"></span>
+                            <span class="fa-regular fa-star text-orange-400"></span>
+                        </h4>
+                    </div>
+                </div>
+                <div class="w-72 text-center mx-auto pt-6">
+                    <a href="">
+                        <h1 class="text-2xl font-bold text-[#282424] hover:underline duration-700">Crafting Engaging CSS Animations step by step guide</h1>
+                    </a>
+                </div>
+                <div class="w-96 text-center mx-auto pt-7">
+                    <p class="text-lg font-medium text-gray-500">We’ve seen the basics of selectors.CSS selectors offer versatile ways to target specific HTML elements for styling….</p>
+                </div>
+                <div class="flex w-72 mx-auto gap-6 items-center   mt-5">
+                    <div class="flex gap-3 items-center">
+                        <img class="w-8 h-8 rounded-full" src="https://ronobirdev.surge.sh/assets/ronobir-cbde17b2.png" alt="">
+                        <h2 class="text-lg font-semibold text-black">Ronobir</h2>
+                    </div>
+                    <div class="flex gap-2 ">
+                        <i class="fa-solid fa-stopwatch text-lg text-[#6A4EE9]"></i>
+
+                        <h2 class="text-lg text-[#6A4EE9] font-medium"> 2 Min Read</h2>
+                    </div>
+                </div>
+
             </div>
         </div>
 
